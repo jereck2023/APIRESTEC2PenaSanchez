4a8a004f3584f1b413b781c9a58ed9e206d8c831
jereck2023 <jereck2020@gmail.com>
2024-04-27 13:30:19 -0500

    Proyecto APIRES evaluacion

---
+++ src/test/java/pe/edu/cibertec/APIRESTEC2PenaSanchez/Apirestec2PenaSanchezApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2PenaSanchez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirestec2PenaSanchezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
